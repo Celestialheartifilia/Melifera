0e564bd62a937373ae6aafa613e88c695697a99b
imvsaltyy <mcxuanning@gmail.com>
2026-03-31 12:05:23 +0800

    UPDATE: highlight flowers

--- Assets/Scripts/Hybriding Flowers/NormalFlower.cs
+++ Assets/Scripts/Hybriding Flowers/NormalFlower.cs
@@ -6,20 +6,33 @@ public class NormalFlower : MonoBehaviour
     public bool isPollinated;
 
     [Header("Speech Bubble")]
-    public GameObject speechIndicator;   // single speech object
+    public GameObject speechIndicator;
+
+    [Header("Hover Sprite")]
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
+    public GameObject soil;
+
+    private SpriteRenderer sr;
 
     void Awake()
     {
+        sr = GetComponent<SpriteRenderer>();
+
         if (speechIndicator != null)
             speechIndicator.SetActive(false);
+
+        // ensure default sprite is set
+        if (sr != null && normalSprite != null)
+            sr.sprite = normalSprite;
     }
 
     public void SetPollinated(bool value)
     {
         isPollinated = value;
         // Later: change sprite / play effect here
-
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<BeeController>() != null)
@@ -38,10 +51,31 @@ public class NormalFlower : MonoBehaviour
         }
     }
 
+    // HOVER START
+    void OnMouseEnter()
+    {
+        if (sr != null && hoverSprite != null)
+        {
+            sr.sprite = hoverSprite;
+            soil.SetActive(false);
+        }
+            
+
+    }
+
+    // HOVER END
+    void OnMouseExit()
+    {
+        if (sr != null && normalSprite != null)
+        {
+            sr.sprite = normalSprite;
+            soil.SetActive(true);
+        }
+    }
+
     void OnMouseDown()
     {
         BeeController bee = FindObjectOfType<BeeController>();
         bee.MoveToFlower(this);
     }
-
 }
